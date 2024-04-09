529259f30f050be5486792f6ae4027f83fb79f94
Jane <tnjeik@gmail.com>
2024-04-09 12:58:44 +0100

    saving before updating to angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-tasks';
+  tasks: any = [];
+
+  async ngOnInit():Promise<void> {
+    try {
+      const request = await fetch("http://localhost:3000/tasks",{ method:'GET' });
+      console.log(request);
+      
+      if(request.ok && request.status === 200) {
+        const response = await request.json()
+        this.tasks = response.data
+        console.log(response);
+        
+      }
+
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
 }
